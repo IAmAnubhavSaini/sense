7fb55b784176a4df18a4165b120f126857b4ec69
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2025-05-04 15:01:28 +0530

    v0.0.13

--- node-pkg/src/checks.ts
+++
@@ -1,4 +0,0 @@
-const isBrowser = () => typeof window !== "undefined" && typeof window.document !== "undefined";
-const isNode = () => typeof process !== "undefined" && process.versions?.node;
-
-export { isBrowser, isNode };
