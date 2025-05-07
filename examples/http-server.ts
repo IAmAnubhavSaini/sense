78dd4cdd9ddff06cd03aeeb3bebccfe1f6168ffa
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2025-05-08 02:30:01 +0530

    Updated examples/http-server with @f0c1s/sense@0.0.17

--- examples/http-server.ts
+++ examples/http-server.ts
@@ -4,29 +4,29 @@ import Sense from "@f0c1s/sense/node";
 const hostname = "127.0.0.1";
 const port = 3000;
 
-/** define is function definition with extra steps */
-Sense.define("handle_request", ([req, res, ts, i]) => {
-    Sense.call("log", [`${i}th: handle_request at ${ts}`]);
+/** on is function definition with extra steps */
+Sense.on("handle_request", ([req, res, ts, i]) => {
+    Sense.need("log", [`${i}th: handle_request at ${ts}`]);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.end("Hello bird");
 });
-Sense.define("full_host", ([port, hostname]) => `Server running at http://${hostname}:${port}/`);
-Sense.define("log", ([value]) => console.log(value));
-Sense.define("listen", ([server, port, hostname]) =>
+Sense.on("full_host", ([port, hostname]) => `Server running at http://${hostname}:${port}/`);
+Sense.on("log", ([value]) => console.log(value));
+Sense.on("listen", ([server, port, hostname]) =>
     server.listen(
         port,
         hostname,
-        async () => await Sense.call("log", [await Sense.call("full_host", [port, hostname])])
+        async () => await Sense.need("log", [await Sense.need("full_host", [port, hostname])])
     )
 );
 
-/** workflow is main() */
-Sense.workflow(async () => {
+/** flow is main() */
+Sense.flow(async () => {
     let i = 0;
     const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-        setTimeout(() => Sense.call("log", [req.url]), 1000);
-        Sense.call("handle_request", [req, res, Date.now(), i++]);
+        setTimeout(() => Sense.need("log", [req.url]), 1000);
+        Sense.need("handle_request", [req, res, Date.now(), i++]);
     });
-    Sense.call("listen", [server, port, hostname]);
+    Sense.need("listen", [server, port, hostname]);
 });
